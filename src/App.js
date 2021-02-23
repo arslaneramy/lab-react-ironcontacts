800b83f96dc76c3911e37a059ac12c47c7ffe036
Arslane Gharout <iamarslane@gmail.com>
2021-02-23 17:53:10 +0100

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import contacts from "./contacts.json";
+// import ButtonToSayHello from "./components/AddRandomContact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    actors: contacts.slice(0, 5),
+  };
+
+
+
+  // handleDelete = (movieId) => {
+
+  //   // Filter the movies from the state to remove one
+  //   const filteredMovies = this.state.movies.filter((movie) => {
+
+  //     if (movie._id !== movieId) {
+  //       return true;  // include the movie in the new array
+  //     }
+  //     else if (movie._id === movieId) {
+  //       return false; // exclude the movie from the new array
+  //     }
+  //     // a bit shorter way:
+  //     // const isMovieToDelete = !movie._id === movieId;
+  //     // return isMovieToDelete
+  //   });
+
+  //   this.setState({ movies: filteredMovies });
+
+  // }
+
+
+
+ addRandom = (actorsId) => {
+   const newArray = [...this.state.actors]
+  const findActor = contacts[Math.floor(Math.random()*contacts.length)]
+  newArray.push(findActor)
+  this.setState({actors : newArray})
+ }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Iron Contact</h1>
+
+        <button onClick={() => {this.addRandom()}}>Add celebrity</button>
+        
+        {this.state.actors.map((contact) => {
+          return (
+            <table>
+              
+              <th>
+                <img className="actorImg" src={contact.pictureUrl} img />{" "}
+              </th>
+
+              <th className="contact">{contact.name}</th>
+              <th>{contact.popularity}</th>
+            </table>
+          );
+        })}
+      </div>
+    );
+  }
+ }
 
 export default App;
---
+++ src/components/AddRandomContact.js
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// const AddRandomContact = (props) => {
+//   let addRandom = (prop) => {
+//     console.log(prop);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={() => addRandom(props.people)}>Add celebrity</button>
+//     </div>
+//   );
+// };
+
+// export default AddRandomContact;
